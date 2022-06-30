fb9cf092ccb598a995a09cdd3887fedac4cb5a0f
himneesh9 <108399860+himneesh9@users.noreply.github.com>
2022-06-30 11:32:58 +0530

    Add files via upload

---
+++ conditional.java
@@ -0,0 +1,15 @@
+public class conditional {
+    public static void main(String[] args) {
+
+    }
+        int age=18;
+        if(age>18){
+            System.out.println("yes");
+        }
+        else {
+            System.out.println("no");
+        }
+
+    }
+
+}
