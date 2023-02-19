a881d454a6db882862666db9a331384f730b8790
himneesh <himneesh9@gmail.com>
2023-02-19 23:35:13 +0530

    anagrams

--- src/string.java
+++ src/string.java
@@ -1,2 +1,21 @@
+ import java.util.*;
 public class string {
+    public static boolean isAnagram(String str, String str1) {
+        if (str.length() != str1.length()) {
+            return false;
+        } else {
+//              str = str.toLowerCase();
+//              str1 = str1.toLowerCase();
+            char[] strarr = str.toCharArray();
+            char[] str1arr = str1.toCharArray();
+
+            Arrays.sort(strarr);
+            Arrays.sort(str1arr);
+            return Arrays.equals(strarr, str1arr);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("race" , "case"));
+    }
 }
